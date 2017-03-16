a42405f5b74abb5344dce307e9f7d53be0f680cd
github username <github email address>
2017-03-16 16:23:16 -0400

    yay i made my donut function

--- js/donut-counter.js
+++ js/donut-counter.js
@@ -1,4 +1,18 @@
 //JS Donut Counter Lab
+function donutCounter(){
+  var numOfGuest = prompt("Please enter number of guest", "enter here");
+  var numOfdonut = prompt("Please enter equired number of donut", "enter here");
+  var guest = parseInt(numOfGuest)
+  var donut = parseInt(numOfdonut)
+
+  if(donut <=  guest){
+    alert("Hello! u have " + guest + "number of guest and"+donut+ "number of donuts,u might need more");
+    // alert(`Hello! u have ${guest} number of guest and ${donut} number of donuts, u might need more`);
+
+  }else {
+    alert("Hello! u have " + guest + "number of guest and"+donut+ "number of donuts");
+  }
+}
 
 //declare 2 variables one for guests and one for donuts
 //use prompt method to bring up a dialog box in the browser for the user to insert the number of guests and donuts.
